d7ef74ad5902a8d3562f2e380ff3126bc067e2a8
InternetUnexplorer <internetunexplorer@gmail.com>
2017-12-02 12:45:09 -0800

    Some changes to __main__.py to help with debugging

--- server/__main__.py
+++ server/__main__.py
@@ -2,30 +2,20 @@ from server.display.widget import *
 
 if __name__ == "__main__":
     pygame.init()
-    pygame.display.set_caption("heyyo")
+    pygame.display.set_caption('Debug')
 
-    t_bar = VerticalBarWidget((0, 0), (16, 55), mode=VerticalBarWidget.MODE_NORMAL, top_text="+Y")
-    b_bar = VerticalBarWidget((0, 0), (16, 55), mode=VerticalBarWidget.MODE_INVERT, bot_text="-Y")
-    r_bar = HorizontalBarWidget((0, 0), (55, 16), mode=HorizontalBarWidget.MODE_NORMAL, left_text=" ", right_text="+X")
-    l_bar = HorizontalBarWidget((0, 0), (55, 16), mode=HorizontalBarWidget.MODE_INVERT, right_text=" ", left_text="-X")
-    bord = BorderLayoutWidget((10, 10), top=t_bar, bottom=b_bar, right=r_bar, left=l_bar, spacing=0)
+    bar_widget = VerticalBarWidget((9, 9), (11, 40), mode=VerticalBarWidget.MODE_NORMAL, top_text='+Y')
+    fps_widget = TextWidget((1247, 750), text='last draw: ??? ms')
     surface = pygame.display.set_mode((1366, 768))
 
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-
-    while True:
-        if pygame.event.wait().type == pygame.QUIT:
-            pygame.quit()
-
-        x = -joystick.get_axis(0) * -1.05
-        y = joystick.get_axis(1) * -1.05
-        x = x if abs(x) >= 0.05 else 0
-        y = y if abs(y) >= 0.05 else 0
-        t_bar.value = y
-        b_bar.value = y
-        r_bar.value = x
-        l_bar.value = x
+    last_draw_time = 0
+    while pygame.event.wait().type != pygame.QUIT:
+        start_time = pygame.time.get_ticks()
         surface.fill((0, 0, 0))
-        bord.blit(surface)
+        fps_widget.text = 'last draw: {:03d} ms'.format(last_draw_time)
+        bar_widget.blit(surface)
+        fps_widget.blit(surface)
         pygame.display.flip()
+        last_draw_time = pygame.time.get_ticks() - start_time
+
+    pygame.quit()
