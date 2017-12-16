21c7a0d43d40b52d64b21ab608de8def67d80f2b
InternetUnexplorer <internetunexplorer@gmail.com>
2017-12-15 17:28:33 -0800

    Improved timer.py

--- common/timer.py
+++ common/timer.py
@@ -1,13 +1,19 @@
-from time import perf_counter
+from time import sleep, perf_counter
 
 
 class Timer:
-    def __init__(self, length: float):
+    def __init__(self, length: float) -> None:
         self.length = length
         self.start_time = perf_counter()
 
-    def is_expired(self):
-        return perf_counter() - self.start_time > self.length
+    def is_expired(self) -> bool:
+        return self.get_remaining_time() > 0
 
-    def restart(self):
+    def get_remaining_time(self) -> int:
+        return max(perf_counter() - self.start_time, 0)
+
+    def wait(self) -> None:
+        sleep(self.get_remaining_time())
+
+    def restart(self) -> None:
         self.start_time = perf_counter()
