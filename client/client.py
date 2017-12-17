80bbf4d04cf8f930b9d332ef585705b21a65e7b0
InternetUnexplorer <internetunexplorer@gmail.com>
2017-12-16 18:08:02 -0800

    Fixed a bug with client.py

--- client/client.py
+++ client/client.py
@@ -1,31 +1,30 @@
 import socket
 import sys
-
-from common.protocol import recv_obj, send_obj
+from time import sleep
 
 
 class Client:
-    MSG_TIMEOUT = 0.1
-    RETRY_DELAY = 0.1
+    CONNECT_TIMEOUT = 1.0
+    RECEIVE_TIMEOUT = 1.0
+    RECONNECT_DELAY = 1.0
 
     def __init__(self, host: str, port: int) -> None:
         self.host = host
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.settimeout(Client.MSG_TIMEOUT)
+        self.connected = False
 
     def connect_and_listen(self) -> None:
-        host = self.host
-        port = self.port
-        sock = self.sock
-        print('  Connecting: {}:{}'.format(host, port))
+        print('Connecting: {}:{}'.format(self.host, self.port))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            sock.connect((host, port))
-            while True:
-                msg = recv_obj(sock, Client.MSG_TIMEOUT)
-                print(msg)
-                send_obj(sock, msg)
+            sock.settimeout(Client.CONNECT_TIMEOUT)
+            sock.connect((self.host, self.port))
+            self.connected = True
+            print('Connected')
         except socket.error as err:
-            print('Disconnected: {}'.format(err), file=sys.stderr)
+            print('Error: {}'.format(err), file=sys.stderr)
         finally:
             sock.close()
+            self.connected = False
+        print('Disconnected (RECONNECT_DELAY: {}s)'.format(Client.RECONNECT_DELAY))
+        sleep(Client.RECONNECT_DELAY)
