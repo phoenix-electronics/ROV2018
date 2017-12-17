aad7d2de7c39472f63b7100a95e032f82b431f9b
InternetUnexplorer <internetunexplorer@gmail.com>
2017-12-16 19:47:03 -0800

    Added start_expired option for Timer

--- common/timer.py
+++ common/timer.py
@@ -2,9 +2,11 @@ from time import sleep, perf_counter
 
 
 class Timer:
-    def __init__(self, length: float) -> None:
+    def __init__(self, length: float, start_expired: bool = False) -> None:
         self.length = length
         self.start_time = perf_counter()
+        if start_expired:
+            self.start_time -= self.length
 
     def is_expired(self) -> bool:
         return self.get_remaining_time() == 0
