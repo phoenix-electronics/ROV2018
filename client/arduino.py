fea9e28f1dedc52e3205e99452d1643a3afaeb96
InternetUnexplorer <internetunexplorer@gmail.com>
2018-03-04 18:31:35 -0800

    Document arduino.py
    Replace deprecated Serial.isOpen() with Serial.is_open in arduino.py

--- client/arduino.py
+++ client/arduino.py
@@ -5,6 +5,8 @@ from serial.tools import list_ports
 
 
 class Arduino:
+    """Wrapper for a serial connection to an Arduino"""
+
     def __init__(self, baud_rate: int = 57600, write_timeout: float = 0.05, enable_dtr: bool = False) -> None:
         self.baud_rate = baud_rate
         self.write_timeout = write_timeout
@@ -12,9 +14,11 @@ class Arduino:
         self.connection = None
 
     def is_connected(self) -> bool:
-        return self.connection is not None and self.connection.isOpen()
+        """Return whether the connection is open"""
+        return self.connection is not None and self.connection.is_open
 
     def connect(self) -> None:
+        """Attempt to detect the port the Arduino is connected to and open a connection"""
         port = self._detect_port()
         if port is not None:
             self.connection = serial.Serial()
@@ -25,10 +29,12 @@ class Arduino:
             self.connection.open()
 
     def write_speeds(self, motor_speeds: Tuple[int, int, int, int, int, int]) -> None:
+        """Write target motor speeds to the Arduino, raising serial.SerialException if the write fails"""
         data = '!{}:{}:{}:{}:{}:{};\n'.format(*motor_speeds)
         self.connection.write(data.encode())
 
     def disconnect(self) -> None:
+        """Close the connection to the Arduino"""
         if self.is_connected():
             self.connection.close()
         self.connection = None
