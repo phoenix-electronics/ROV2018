468c1f4d83c5d86672338ce78f7d72fc26474a46
InternetUnexplorer <internetunexplorer@gmail.com>
2018-03-15 12:19:07 -0700

    Update client/__main__.py, client/client.py

--- client/__main__.py
+++ client/__main__.py
@@ -1,3 +1,23 @@
+import os
+from time import sleep
+
+from client.arduino import Arduino
+from client.camera_stream import CameraStream
 from client.client import Client
 
-Client('localhost', 768).connect_and_listen()
+if __name__ == '__main__':
+    # Read server address and port from environment
+    host = os.getenv('HOST', 'localhost')
+    port = int(os.getenv('PORT', '1234'))
+
+    # Initialize Arduino connection and video stream
+    arduino = Arduino()
+    camera_stream = CameraStream()
+    arduino.connect()
+    camera_stream.set_source(0)
+
+    # Create and run client
+    client = Client(host, port, arduino, camera_stream)
+    while True:
+        client.connect_and_run()
+        sleep(client.reconnect_delay)
--- client/client.py
+++ client/client.py
@@ -1,30 +1,79 @@
 import socket
-import sys
-from time import sleep
+
+import serial
+
+import common.logging as logger
+from client.arduino import Arduino
+from client.camera_stream import CameraStream
+from client.system_info import SystemInfo
+from common.command import SetMotorSpeedsCommand, SetCameraCommand
+from common.message import ArduinoConnectionMessage, SystemInfoMessage
+from common.protocol import send_obj, recv_obj
+from common.timer import Timer
 
 
 class Client:
-    CONNECT_TIMEOUT = 1.0
-    RECEIVE_TIMEOUT = 1.0
-    RECONNECT_DELAY = 1.0
-
-    def __init__(self, host: str, port: int) -> None:
+    def __init__(self, host: str, port: int, arduino: Arduino, camera_stream: CameraStream, sock_timeout: float = 0.5,
+                 reconnect_delay: float = 1.0, send_system_info_interval: float = 1.0) -> None:
         self.host = host
         self.port = port
-        self.connected = False
+        self.arduino = arduino
+        self.camera_stream = camera_stream
 
-    def connect_and_listen(self) -> None:
-        print('Connecting: {}:{}'.format(self.host, self.port))
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_timeout = sock_timeout
+        self.reconnect_delay = reconnect_delay
+        self.send_system_info_interval = send_system_info_interval
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(self.sock_timeout)
+
+    def connect_and_run(self) -> None:
+        """Connect to the server and run the client"""
+        logger.info('Connecting to server: {}:{}', self.host, self.port)
         try:
-            sock.settimeout(Client.CONNECT_TIMEOUT)
-            sock.connect((self.host, self.port))
-            self.connected = True
-            print('Connected')
+            self.sock.connect((self.host, self.port))
         except socket.error as err:
-            print('Error: {}'.format(err), file=sys.stderr)
+            logger.error('Unable to connect: {} (retrying in {}s)', err, self.reconnect_delay)
+            return
+        logger.info("Connected to server")
+        # Initialize timer to periodically send a SystemInfoMessage
+        send_system_info_timer = Timer(self.send_system_info_interval, start_expired=True)
+        try:
+            # Inform the server of the current state of the Arduino connection
+            send_obj(self.sock, ArduinoConnectionMessage(self.arduino.is_connected()))
+            while True:
+                # Send a SystemInfoMessage if send_stats_interval has elapsed
+                if send_system_info_timer.is_expired():
+                    send_obj(self.sock, SystemInfoMessage(SystemInfo()))
+                    send_system_info_timer.restart()
+                # Receive and handle command
+                command = recv_obj(self.sock, self.sock_timeout)
+                self.handle_command(command)
+        except socket.error as err:
+            logger.error('Connection closed: {} (reconnecting in {}s)', err, self.reconnect_delay)
         finally:
-            sock.close()
-            self.connected = False
-        print('Disconnected (RECONNECT_DELAY: {}s)'.format(Client.RECONNECT_DELAY))
-        sleep(Client.RECONNECT_DELAY)
+            self.sock.close()
+            # If the Arduino is connected, try and stop the motors
+            if self.arduino.is_connected():
+                try:
+                    self.arduino.write_speeds((1500, 1500, 1500, 1500, 1500, 1500))
+                except serial.SerialException:
+                    pass
+
+    def handle_command(self, command: object) -> None:
+        """Handle command from the server"""
+        if isinstance(command, SetMotorSpeedsCommand):  # Server requested new motor speeds
+            # Try and connect to the Arduino if it is not connected
+            if not self.arduino.is_connected() and self.arduino.connect():
+                # Inform the server that the Arduino connection state has changed
+                send_obj(self.sock, ArduinoConnectionMessage(True))
+            # Try and write the motor speeds to the Arduino if it is connected
+            if self.arduino.is_connected():
+                try:
+                    self.arduino.write_speeds(command.motor_speeds)
+                except serial.SerialException:
+                    # Error writing motor speeds, disconnect and inform the server
+                    self.arduino.disconnect()
+                    send_obj(self.sock, ArduinoConnectionMessage(False))
+        elif isinstance(command, SetCameraCommand):  # Server requested new camera index
+            self.camera_stream.set_source(command.camera_index)
