0f3cb450968a0f551299715859ac85a1cb382d00
InternetUnexplorer <internetunexplorer@gmail.com>
2018-03-20 13:57:21 -0700

    Update joystick.py

--- server/joystick.py
+++ server/joystick.py
@@ -3,6 +3,15 @@ from typing import List, Tuple
 import pygame
 
 
+class JoystickData:
+    """Container for data read from the joystick"""
+
+    def __init__(self, axes: List[float], buttons: List[bool], hat: Tuple[int, int]) -> None:
+        self.axes = axes
+        self.buttons = buttons
+        self.hat = hat
+
+
 class Joystick:
     """Wrapper for a Pygame joystick connection"""
 
@@ -10,23 +19,26 @@ class Joystick:
         self.joystick = None
 
     def is_connected(self) -> bool:
-        """Return whether the connection is open"""
+        """Return whether the joystick is connected"""
         return self.joystick is not None
 
-    def connect(self) -> None:
+    def connect(self) -> bool:
         """Attempt to connect to the joystick"""
         if pygame.joystick.get_count() > 0:
             self.joystick = pygame.joystick.Joystick(0)
             self.joystick.init()
+            self.read_values()
+            return True
+        return False
 
     def disconnect(self) -> None:
         """Close the connection to the joystick"""
         self.joystick.quit()
         self.joystick = None
 
-    def read_values(self) -> Tuple[List[float], List[bool], Tuple[int, int]]:
-        """Read the values of the axes, buttons, and hat from the joystick"""
+    def read_values(self) -> JoystickData:
+        """Read the values of the joystick's axes, buttons, and hat, and return a JoystickData"""
         axes = [self.joystick.get_axis(i) for i in range(self.joystick.get_numaxes())]
         buttons = [self.joystick.get_button(i) for i in range(self.joystick.get_numbuttons())]
         hat = self.joystick.get_hat(0)
-        return axes, buttons, hat
+        return JoystickData(axes, buttons, hat)
