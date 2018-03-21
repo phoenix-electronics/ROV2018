d717163adf62941411ec9b4f513b63f7a8e0731a
InternetUnexplorer <internetunexplorer@gmail.com>
2018-03-21 15:13:13 -0700

    Implement __repr__ for Command and Message classes

--- common/command.py
+++ common/command.py
@@ -9,9 +9,15 @@ class SetMotorSpeedsCommand:
     def __init__(self, motor_speeds: Tuple[int, int, int, int, int, int]) -> None:
         self.motor_speeds = motor_speeds
 
+    def __repr__(self) -> str:
+        return 'SetMotorSpeedsCommand(motor_speeds={})'.format(self.motor_speeds)
+
 
 class SetCameraCommand:
     """Set the camera used for the video stream"""
 
     def __init__(self, camera_index: int) -> None:
         self.camera_index = camera_index
+
+    def __repr__(self) -> str:
+        return 'SetCameraCommand(camera_index={})'.format(self.camera_index)
--- common/message.py
+++ common/message.py
@@ -11,9 +11,16 @@ class SystemInfoMessage:
         self.cpu_temp = cpu_temp
         self.mem_usage = mem_usage
 
+    def __repr__(self) -> str:
+        return 'SystemInfoMessage(cpu_usage={}, cpu_temp={}, mem_usage={})' \
+            .format(self.cpu_usage, self.cpu_temp, self.mem_usage)
+
 
 class ArduinoConnectionMessage:
     """Information about the state of the Arduino connection"""
 
     def __init__(self, connected: bool) -> None:
         self.connected = connected
+
+    def __repr__(self) -> str:
+        return 'ArduinoConnectionMessage(connected={})'.format(self.connected)
