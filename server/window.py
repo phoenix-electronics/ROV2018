9102a7b0ef1f5e468ebd84e1b9c5e13c0de7620e
InternetUnexplorer <internetunexplorer@gmail.com>
2018-04-03 12:31:55 -0700

    Add window.py

---
+++ server/window.py
@@ -0,0 +1,27 @@
+import pygame
+
+from common import logging
+
+
+class Window:
+    def __init__(self) -> None:
+        self.surface = None
+
+    def show(self) -> None:
+        """Show the window"""
+        pygame.display.init()
+        self.surface = pygame.display.set_mode((324, 768))
+        self.update()
+
+    def is_showing(self) -> bool:
+        return self.surface is not None
+
+    def update(self) -> None:
+        """Update the contents of the window"""
+        self.surface.fill((0, 0, 0))
+        pygame.display.flip()
+
+    def hide(self) -> None:
+        """Hide the window"""
+        self.surface = None
+        pygame.display.quit()
