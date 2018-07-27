44cf83d942933f88ab2492f14fb0693dcef7cec7
InternetUnexplorer <internetunexplorer@gmail.com>
2018-07-27 10:25:10 +0200

    Update widget.py

--- server/widget.py
+++ server/widget.py
@@ -1,20 +1,25 @@
+from math import ceil
 from typing import Tuple, List
 
 import pygame
 
 
 class Widget:
-    DEFAULT_COLOR = (255, 255, 255)
+    """Base widget class"""
+
+    DEFAULT_FG_COLOR = (255, 255, 255)
+    DEFAULT_BG_COLOR = (0, 0, 0)
 
     def __init__(self, pos: Tuple[int, int], size: Tuple[int, int], children: List['Widget'] = None) -> None:
         self.pos = pos
         self.size = size
-        self.color = Widget.DEFAULT_COLOR
+        self.color = self.DEFAULT_FG_COLOR
         self.visible = True
         self.surface = pygame.Surface(size, pygame.SRCALPHA)
-        self.children = children if children else []
+        self.children = children if children is not None else []
 
     def blit(self, surface: pygame.Surface) -> None:
+        """Recursively draw the widget and its children, and blit the result onto the provided surface"""
         if self.visible:
             self.surface.fill((0, 0, 0, 0))
             self.draw()
@@ -32,10 +37,13 @@ class Widget:
         return self._scx(x, margin), self._scy(y, margin)
 
     def draw(self) -> None:
+        """Draw the widget onto its surface"""
         pass
 
 
 class VerticalLayoutWidget(Widget):
+    """Widget that lays out its children in a single vertical row"""
+
     ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT = range(3)
 
     def __init__(self, pos: Tuple[int, int], children: [Widget], alignment: int = ALIGN_CENTER,
@@ -53,6 +61,8 @@ class VerticalLayoutWidget(Widget):
 
 
 class HorizontalLayoutWidget(Widget):
+    """Widget that lays out its children in a single horizontal row"""
+
     ALIGN_TOP, ALIGN_MIDDLE, ALIGN_BOTTOM = range(3)
 
     def __init__(self, pos: Tuple[int, int], children: [Widget], alignment: int = ALIGN_MIDDLE,
@@ -70,6 +80,8 @@ class HorizontalLayoutWidget(Widget):
 
 
 class BorderLayoutWidget(Widget):
+    """Widget that lays out its children in top, left, right, bottom, and center positions"""
+
     def __init__(self, pos: Tuple[int, int], top: Widget = None, right: Widget = None, bottom: Widget = None,
                  left: Widget = None, center: Widget = None, spacing: int = 0) -> None:
         h_widget = HorizontalLayoutWidget((0, 0), list(filter(None, (left, center, right))), spacing=spacing)
@@ -78,25 +90,33 @@ class BorderLayoutWidget(Widget):
 
 
 class TextWidget(Widget):
+    """Widget that displays text"""
+
+    FONT_SM = ('Roboto Mono Medium', 11)
+    FONT_MD = ('Roboto Mono Medium', 13)
+    FONT_LG = ('Roboto Mono Medium', 15)
+
     _loaded_fonts = {}
 
-    def __init__(self, pos: Tuple[int, int], text: str, font_size: int = 15) -> None:
-        if font_size not in self._loaded_fonts:
-            self._loaded_fonts[font_size] = pygame.font.SysFont('Arial', font_size)
-        self.font = self._loaded_fonts[font_size]
+    def __init__(self, pos: Tuple[int, int], text: str, font: Tuple[str, int] = FONT_MD) -> None:
+        if font not in self._loaded_fonts:
+            self._loaded_fonts[font] = pygame.font.SysFont(*font)
+        self.font = self._loaded_fonts[font]
         self.text = text
-        self._last_drawn_text = None
+        self._text = None
         self.text_surface = None
         super().__init__(pos, self.font.size(text))
 
     def draw(self) -> None:
-        if self.text != self._last_drawn_text:
-            self.text_surface = self.font.render(self.text, False, self.color)
-            self._last_drawn_text = self.text
+        if self.text != self._text:
+            self.text_surface = self.font.render(self.text, True, self.color)
+            self._text = self.text
         self.surface.blit(self.text_surface, (0, 0))
 
 
 class GlyphWidget(Widget):
+    """Widget that displays a glyph"""
+
     GLYPH_NONE, GLYPH_ERROR, GLYPH_SQUARE = range(3)
 
     def __init__(self, pos: Tuple[int, int], size: int, glyph: int, margin: int = 0) -> None:
@@ -115,8 +135,15 @@ class GlyphWidget(Widget):
 
 
 class BarWidget(Widget):
-    def __init__(self, pos: Tuple[int, int], size: Tuple[int, int], border: Tuple[bool, bool, bool, bool]) -> None:
-        self.border = border
+    """Widget that displays a fillable bar with optional borders and margin"""
+
+    FROM_TOP, FROM_RIGHT, FROM_BOTTOM, FROM_LEFT = range(4)
+
+    def __init__(self, pos: Tuple[int, int], size: Tuple[int, int], direction: int = FROM_LEFT,
+                 border: Tuple[bool, bool, bool, bool] = None, margin: int = 2) -> None:
+        self.direction = direction
+        self.border = [(True, False) * 2, (False, True) * 2][direction % 2] if border is None else border
+        self.margin = tuple([int(b) * margin for b in self.border])
         self.value = None
         glyph_size = min(*size)
         glyph_x, glyph_y = (size[0] - glyph_size) // 2, (size[1] - glyph_size) // 2
@@ -124,96 +151,30 @@ class BarWidget(Widget):
         super().__init__(pos, size, [self.glyph])
 
     def draw(self) -> None:
+        surface, color, direction, margin, value = self.surface, self.color, self.direction, self.margin, self.value
+        width, height = self.size
         border_points = [(0, 0), (1, 0), (1, 1), (0, 1)]
         for side, border in enumerate(self.border):
             if border:
                 line_start, line_end = border_points[side], border_points[(side + 1) % 4]
                 pygame.draw.line(self.surface, self.color, self._sc(*line_start), self._sc(*line_end))
+        if self.value:
+            max_bar_w = (width - margin[1] - margin[3])
+            max_bar_h = (height - margin[0] - margin[2])
+            x = int(margin[3] + ((1 - value) * max_bar_w if direction == self.FROM_RIGHT else 0))
+            y = int(margin[0] + ((1 - value) * max_bar_h if direction == self.FROM_BOTTOM else 0))
+            w = ceil(value * max_bar_w if direction == self.FROM_LEFT else width - margin[1] - x)
+            h = ceil(value * max_bar_h if direction == self.FROM_TOP else height - margin[0] - y)
+            pygame.draw.rect(surface, color, (x, y, w, h))
         self.glyph.visible = self.value is None
 
 
-class VerticalBarWidget(Widget):
-    MODE_NORMAL, MODE_CENTER, MODE_INVERT = range(3)
-
-    def __init__(self, pos: Tuple[int, int], size: Tuple[int, int], mode: int = MODE_NORMAL, top_text: str = None,
-                 bot_text: str = None, border: Tuple[bool, bool, bool, bool] = None) -> None:
-        children = []
-        if top_text:
-            children.append(TextWidget((0, 0), top_text))
-        self.bar = BarWidget((0, 0), size, border or (True, False, True, False))
-        children.append(self.bar)
-        if bot_text:
-            children.append(TextWidget((0, 0), bot_text))
-        parent = VerticalLayoutWidget((0, 0), children=children, spacing=0)
-        self.mode, self.value = mode, None
-        super().__init__(pos, parent.size, children=[parent])
-
-    def draw(self):
-        mode, raw_value = self.mode, self.value
-        self.bar.value = raw_value
-        if raw_value is not None:
-            v_min, v_max = [(0, 1), (-1, 1), (-1, 0)][mode]
-            value = max(v_min, min(v_max, raw_value))
-            surface, color = self.surface, self.color
-            width, height = self.bar.size
-            x_max, y_max = width - 1, height - 1
-            v_padding = 2
-            rect_x = self.bar.pos[0]
-            rect_y = [y_max, y_max // 2, 0][mode] + self.bar.pos[1]
-            rect_w = width
-            rect_h = int((y_max // (v_max - v_min) - 2 * v_padding) * abs(value))
-            if value > 0:
-                pygame.draw.rect(surface, color, (rect_x, rect_y - v_padding, rect_w, -rect_h + 1))
-            elif value < 0:
-                pygame.draw.rect(surface, color, (rect_x, rect_y + v_padding, rect_w, rect_h + 1))
-            if mode == VerticalBarWidget.MODE_CENTER:
-                pygame.draw.rect(surface, color, (rect_x, rect_y, width, 1))
-
-
-class HorizontalBarWidget(Widget):
-    MODE_NORMAL, MODE_CENTER, MODE_INVERT = range(3)
-
-    def __init__(self, pos: Tuple[int, int], size: Tuple[int, int], mode: int = MODE_NORMAL, left_text: str = None,
-                 right_text: str = None, border: Tuple[bool, bool, bool, bool] = None) -> None:
-        children = []
-        if left_text:
-            children.append(TextWidget((0, 0), left_text))
-        self.bar = BarWidget((0, 0), size, border or (False, True, False, True))
-        children.append(self.bar)
-        if right_text:
-            children.append(TextWidget((0, 0), right_text))
-        parent = HorizontalLayoutWidget((0, 0), children=children, spacing=2)
-        self.mode, self.value = mode, None
-        super().__init__(pos, parent.size, children=[parent])
-
-    def draw(self) -> None:
-        mode, raw_value = self.mode, self.value
-        self.bar.value = raw_value
-        if raw_value is not None:
-            h_min, h_max = [(0, 1), (-1, 1), (-1, 0)][mode]
-            value = max(h_min, min(h_max, raw_value))
-            surface, color = self.surface, self.color
-            width, height = self.bar.size
-            x_max, y_max = width - 1, height - 1
-            h_padding = 2
-            rect_x = [0, x_max // 2, x_max][mode] + self.bar.pos[0]
-            rect_y = self.bar.pos[1]
-            rect_w = int((x_max // (h_max - h_min) - 2 * h_padding) * abs(value))
-            rect_h = height
-            if value > 0:
-                pygame.draw.rect(surface, color, (rect_x + h_padding, rect_y, rect_w + 1, rect_h))
-            elif value < 0:
-                pygame.draw.rect(surface, color, (rect_x - h_padding, rect_y, -rect_w + 1, rect_h))
-            if mode == VerticalBarWidget.MODE_CENTER:
-                pygame.draw.rect(surface, color, (rect_x, rect_y, 1, height))
-
-
 class LabeledContainerWidget(Widget):
-    DEFAULT_LABEL_COLOR = (0, 0, 0)
+    """Widget that displays a child widget with a title bar"""
 
     def __init__(self, pos: Tuple[int, int], child: Widget, text: str, padding: int = 4) -> None:
-        self.label = TextWidget((5, 2), text, font_size=11)
-        self.label.color = self.DEFAULT_LABEL_COLOR
+        self.label = TextWidget((5, 2), text, TextWidget.FONT_SM)
+        self.label.color = self.DEFAULT_BG_COLOR
         self.padding = padding
         label_w, label_h = self.label.size
         child_w, child_h = child.size
@@ -224,7 +185,7 @@ class LabeledContainerWidget(Widget):
 
     def draw(self) -> None:
         surface, color = self.surface, self.color
-        label_w, label_h = self.label.size
+        label_height = self.label.size[1]
         width, height = self.size
-        pygame.draw.rect(surface, color, (0, 0, label_w + 8, label_h + 4))
-        pygame.draw.rect(surface, color, (0, label_h + 4, width, height - label_h - 4), 1)
+        pygame.draw.rect(surface, color, (0, 0, width, label_height + 4))
+        pygame.draw.rect(surface, color, (0, label_height + 4, width, height - label_height - 4), 1)
