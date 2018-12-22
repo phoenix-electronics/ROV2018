440c93057a294cc69f99a045e1fca5e9470ec34c
InternetUnexplorer <internetunexplorer@gmail.com>
2018-12-21 16:48:11 -0800

    Update arduino_upload.sh, client.sh, server.sh

--- arduino_upload.sh
+++ arduino_upload.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
+##
+## This script builds and uploads `arduino/arduino.ino` to the connected board.
+##
+
 export ARDUINO_PORT=/dev/ttyUSB0
-export ARDUINO_BOARD=arduino:avr:nano:cpu=atmega328
+export ARDUINO_FQBN=arduino:avr:nano:cpu=atmega328
 
 echo "----------------"
 echo "ARDUINO_PORT=$ARDUINO_PORT"
-echo "ARDUINO_BOARD=$ARDUINO_BOARD"
+echo "ARDUINO_FQBN=$ARDUINO_FQBN"
 echo "----------------"
 
-arduino --upload arduino/arduino.ino --port $ARDUINO_PORT --board $ARDUINO_BOARD
+arduino --upload arduino/arduino.ino --port $ARDUINO_PORT --board $ARDUINO_FQBN
--- server.sh
+++ server.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env sh
 
-export HOST=192.168.0.1
+##
+## This script starts the server and the GStreamer pipeline.
+##
+
+export HOST=localhost
 export PORT=1234
+export GST_PORT=5000
+#export JOYSTICK_INDEX=0
+#export ENABLE_JOYSTICK_HOTPLUG=1
 
 echo "----------------"
 echo "HOST=$HOST"
 echo "PORT=$PORT"
+echo "GST_PORT=$GST_PORT"
 echo "----------------"
 
-gst-launch-1.0 udpsrc port=5000 \
+gst-launch-1.0 udpsrc port=$GST_PORT \
     ! application/x-rtp,encoding-name=JPEG,payload=26 \
     ! rtpjpegdepay \
     ! jpegdec \
